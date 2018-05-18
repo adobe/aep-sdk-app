5a9f6ebed3debd1cb68614fb78b1f7692b5243b4
Vadymus <vadym_us@yahoo.com>
2018-05-18 10:47:21 -0400

    Initial Commit

---
+++ T-Mobile Adobe MC POC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T-Mobile Adobe MC POC
+//
+//  Created by ustymenk on 5/18/18.
+//  Copyright © 2018 VUES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ T-Mobile Adobe MC POCUITests/T_Mobile_Adobe_MC_POCUITests.swift
@@ -0,0 +1,36 @@
+//
+//  T_Mobile_Adobe_MC_POCUITests.swift
+//  T-Mobile Adobe MC POCUITests
+//
+//  Created by ustymenk on 5/18/18.
+//  Copyright © 2018 VUES. All rights reserved.
+//
+
+import XCTest
+
+class T_Mobile_Adobe_MC_POCUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
